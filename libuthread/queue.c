1dbd8b1decd8da684a81ca27bec6ebf9efd710bc
richaisrani <56519259+richaisrani@users.noreply.github.com>
2019-10-29 23:04:39 -0700

    Update queue.c

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -8,9 +8,9 @@
 
 /*doubly linked list implementation of queue
 q_node stores a pointer to the next node as well as previous*/
-struct q_node {         
-    void * node_data;       
-    struct q_node * next;       
+struct q_node {
+    void * node_data;
+    struct q_node * next;
     struct q_node * previous;
 };
 
@@ -22,13 +22,13 @@ struct queue {
 //allocate memory and initializes first and last as NULL
 queue_t queue_create(void) {
     struct queue * new_q = (struct queue * ) malloc(sizeof(struct queue));
-    new_q - > first = NULL;
-    new_q - > last = NULL;
+    new_q->first = NULL;
+    new_q->last = NULL;
 
     return new_q;
 }
 
-/*deallocates memory from every node 
+/*deallocates memory from every node
 then deallocates the queue itself
 if the queue is destroyed successfully, return 0
 if no queue to destroy, return -1*/
@@ -40,17 +40,17 @@ int queue_destroy(queue_t queue) {
         return -1;
     }
 
-    current_node = queue - > first;
-    next_node = current_node - > next;
+    current_node = queue->first;
+    next_node = current_node->next;
     while (next_node != NULL) {
         free(current_node);
         current_node = next_node;
-        next_node = current_node - > next;
+        next_node = current_node->next;
     };
     free(current_node);
 
-    queue - > first = NULL;
-    queue - > last = NULL;
+    queue->first = NULL;
+    queue->last = NULL;
 
     free(queue);
 
@@ -71,41 +71,41 @@ int queue_enqueue(queue_t queue, void * data) {
         return -1;
     }
 
-    new_node - > node_data = data;
-    new_node - > next = NULL;
-    new_node - > previous = NULL;
+    new_node->node_data = data;
+    new_node->next = NULL;
+    new_node->previous = NULL;
 
-    if (queue - > last == NULL) {
-        queue - > first = new_node;
-        queue - > last = new_node;
+    if (queue->last == NULL) {
+        queue->first = new_node;
+        queue->last = new_node;
 
         return 0;
     }
 
-    new_node - > previous = queue - > last;
-    queue - > last - > next = new_node;
-    queue - > last = new_node;
+    new_node->previous = queue->last;
+    queue->last->next = new_node;
+    queue->last = new_node;
 
     return 0;
 }
 
-/*Dequeues the oldest node from the queue 
+/*Dequeues the oldest node from the queue
 if there is only one node, it is removed and both first and last set to NULL
 Returns the data item of the dequeued node by reference and function returns 0 if successfull*/
 int queue_dequeue(queue_t queue, void ** data) {
-    if (queue == NULL || queue - > first == NULL || queue - > first - > node_data == NULL) {
+    if (queue == NULL || queue->first == NULL || queue->first->node_data == NULL) {
         return -1;
     }
 
-    struct q_node * current = queue - > first;
-    * data = current - > node_data;
+    struct q_node * current = queue->first;
+    * data = current->node_data;
 
-    if (queue - > first == queue - > last) {
-        queue - > first = NULL;
-        queue - > last = NULL;
+    if (queue->first == queue->last) {
+        queue->first = NULL;
+        queue->last = NULL;
     } else {
-        queue - > first = queue - > first - > next;
-        queue - > first - > previous = NULL;
+        queue->first = queue->first->next;
+        queue->first->previous = NULL;
     }
 
     free(current);
@@ -120,8 +120,8 @@ if data found at last node, sets the previous node to last node
 if data in middle, updates previous and next nodes*/
 int queue_delete(queue_t queue, void * data) {
 
-    struct q_node * current = queue - > first;
-   
+    struct q_node * current = queue->first;
+
     int q_length = queue_length(queue);
 
     if (queue == NULL) {
@@ -129,27 +129,27 @@ int queue_delete(queue_t queue, void * data) {
     }
 
     for (int i = 0; i < q_length; i++) {
-        if (current - > node_data == data) {
+        if (current->node_data == data) {
 
-           
 
-            if (current == queue - > first) { 
-                current - > next - > previous = NULL;
-                queue - > first = current - > next;
 
-            } else if (current == queue - > last) { 
-                current - > previous - > next = NULL;
-                queue - > last = current - > previous;
+            if (current == queue->first) {
+                current->next->previous = NULL;
+                queue->first = current->next;
 
-            } else { 
-                current - > previous - > next = current - > next;
-                current - > next - > previous = current - > previous;
+            } else if (current == queue->last) {
+                current->previous->next = NULL;
+                queue->last = current->previous;
+
+            } else {
+                current->previous->next = current->next;
+                current->next->previous = current->previous;
             }
 
             free(current);
             return 0;
         }
-        current = current - > next;
+        current = current->next;
     }
 
     return -1;
@@ -165,19 +165,19 @@ int queue_iterate(queue_t queue, queue_func_t func, void * arg, void ** data) {
 
     int retval = 0;
 
-    struct q_node * current = queue - > first;
+    struct q_node * current = queue->first;
 
     int len = queue_length(queue);
 
     for (int i = 0; i < len; i++) {
-        retval = func(current - > node_data, arg);
+        retval = func(current->node_data, arg);
         if (retval == 1) {
             if (data != NULL) {
-                * data = current - > node_data;
+                * data = current->node_data;
             }
             return 0;
         }
-        current = current - > next;
+        current = current->next;
     }
 
     return 0;
@@ -194,19 +194,17 @@ int queue_length(queue_t queue) {
         return -1;
     }
 
-    if (queue - > last == NULL) {
+    if (queue->last == NULL) {
         return 0;
     }
 
-    current = queue - > first;
+    current = queue->first;
     q_size++;
 
-    for (; current - > next != NULL; q_size++) {
-        current = current - > next;
+    for (; current->next != NULL; q_size++) {
+        current = current->next;
     }
 
     return q_size;
 
 }
-
-
